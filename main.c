f0de584435ab78788ea47927c1e5d15de4732966
Shisoo64 <shisoo64@gmail.com>
2022-04-23 22:18:35 +0200

    cool

--- main.c
+++ main.c
@@ -9,9 +9,10 @@ int	ft_putchar(char c)
 	return (1);
 }
 
-void	ft_putnbr(int nb)
+int	ft_putnbr(int nb)
 {
         unsigned int    buf;
+	static int	len;
 
         buf = nb;
         if (nb < 0)
@@ -20,12 +21,13 @@ void	ft_putnbr(int nb)
                 write(1, "-", 1);
         }
         if (buf <= 9)
-		ft_putchar(buf + '0');
+		len += ft_putchar(buf + '0');
 	if (buf > 9)
 	{
 		ft_putnbr(buf / 10);
 		ft_putnbr(buf % 10);
 	}
+	return (len);
 }
 
 int	ft_putstr(char *str)
@@ -36,6 +38,22 @@ int	ft_putstr(char *str)
 	while (str[++i] != '\0')
 		write(1, &str[i], 1);
 	return (i);
+}
+
+int	ft_params (char format, va_list args)
+{
+	int	len;
+
+	len = 0;
+	if (format == 's')
+		len += ft_putstr(va_arg( args, char * ));
+	if (format == 'i')
+		len += ft_putnbr(va_arg( args, int ));
+	if (format == 'c')
+		len += ft_putchar((char)va_arg( args, int ));
+	if (format == '%')
+               len += ft_putchar('%');
+	return (len);
 }
 
 int	ft_printf(const char *str, ...)
@@ -52,17 +70,7 @@ int	ft_printf(const char *str, ...)
 		if (str[i] != '%')
 			len += ft_putchar(str[i]);
 		else
-		{
-			if (str[i] == '%' && str[i + 1] == 's')
-                		len += ft_putstr(va_arg( args, char * ));
-			if (str[i] == '%' && str[i + 1] == 'i')
-                		ft_putnbr(va_arg( args, int ));
-			if (str[i] == '%' && str[i + 1] == 'c')
-                		len += ft_putchar((char)va_arg( args, int ));
-			if (str[i] == '%' && str[i + 1] == '%')
-                		len += ft_putchar('%');
-			i++;
-		}
+			len += ft_params(str[++i], args);
 	}
 	va_end(args);
 	return (len);
@@ -71,7 +79,7 @@ int	ft_printf(const char *str, ...)
 int	main (void)
 {
 	ft_printf("\nWritten char: %i\n\n", ft_printf("str: %s", "salut les gars") );
-	ft_printf("\nX Written char: %i\n\n", ft_printf("int: %i", 123) );
+	ft_printf("\nWritten char: %i\n\n", ft_printf("int: %i", 1234567) );
 	ft_printf("\nWritten char: %i\n\n", ft_printf("chr: %c", 'x') );
 	ft_printf("\nWritten char: %i\n\n", ft_printf("per: %%") );
 	return (0);
