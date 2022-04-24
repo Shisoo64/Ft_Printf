6dc671b374ab8dfc84d9614eba8d0967644a92fc
Shisoo64 <shisoo64@gmail.com>
2022-04-24 15:53:39 +0200

    Nearly done

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+size_t  ft_strlen(const char *s)
+{
+        size_t  i;
+
+        i = 0;
+        while (s[i++])
+                ;
+        return (--i);
+}
+
 int	ft_putchar(char c)
 {
 	write(1, &c, 1);
@@ -30,6 +40,29 @@ int	ft_putnbr(int nb)
 	return (len);
 }
 
+int	ft_putnbr_base(long nb, char *base)
+{
+	unsigned int    buf;
+	static int	len;
+	unsigned int	baselen;
+
+	baselen = ft_strlen(base);
+	buf = nb;
+	if (nb < 0)
+	{
+		buf = -buf;
+		len += ft_putchar('-');
+	}
+	if (buf <= baselen - 1)
+		len += ft_putchar(base[buf]);
+	if (buf > baselen - 1)
+	{
+		ft_putnbr_base(buf / baselen, base);
+		ft_putnbr_base(buf % baselen, base);
+	}
+	return (len);
+}
+
 int	ft_putstr(char *str)
 {
 	int	i;
@@ -45,12 +78,18 @@ int	ft_params (char format, va_list args)
 	int	len;
 
 	len = 0;
+	if (format == 'c')
+		len += ft_putchar((char)va_arg( args, int ));
 	if (format == 's')
 		len += ft_putstr(va_arg( args, char * ));
 	if (format == 'i')
-		len += ft_putnbr(va_arg( args, int ));
-	if (format == 'c')
-		len += ft_putchar((char)va_arg( args, int ));
+		len += ft_putnbr_base(va_arg( args, int ), "0123456789");
+	if (format == 'u')
+		len += ft_putnbr_base(va_arg( args, unsigned int ), "0123456789");
+	if (format == 'x')
+		len += ft_putnbr_base(va_arg( args, int ), "0123456789abcdef");
+	if (format == 'X')
+		len += ft_putnbr_base(va_arg( args, int ), "0123456789ABCDEF");
 	if (format == '%')
                len += ft_putchar('%');
 	return (len);
@@ -78,9 +117,12 @@ int	ft_printf(const char *str, ...)
 
 int	main (void)
 {
-	ft_printf("\nWritten char: %i\n\n", ft_printf("str: %s", "salut les gars") );
-	ft_printf("\nWritten char: %i\n\n", ft_printf("int: %i", 1234567) );
 	ft_printf("\nWritten char: %i\n\n", ft_printf("chr: %c", 'x') );
+	ft_printf("\nWritten char: %i\n\n", ft_printf("str: %s", "salut les gars") );
+	ft_printf("\nWritten char: %i\n\n", ft_printf("uns: %u", 4294967295) );
+	ft_printf("\nWritten char: %i\n\n", ft_printf("int: %i", 2147483647) );
+	ft_printf("\nWritten char: %i\n\n", ft_printf("hex: %x", 2147483647) );
+	ft_printf("\nWritten char: %i\n\n", ft_printf("HEX: %X", 2147483647) );
 	ft_printf("\nWritten char: %i\n\n", ft_printf("per: %%") );
 	return (0);
 }
