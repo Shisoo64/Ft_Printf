3f8d3f06759fafbf14e48c0eaa30401bfbc92e4f
Romil <47531544+Shisoo64@users.noreply.github.com>
2022-04-27 16:16:51 +0200

    Normed 100%

--- ft_printf.h
+++ ft_printf.h
@@ -6,26 +6,26 @@
 /*   By: rlaforge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 16:20:10 by rlaforge          #+#    #+#             */
-/*   Updated: 2022/04/27 14:58:11 by rlaforge         ###   ########.fr       */
+/*   Updated: 2022/04/27 16:14:58 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINFT_H
+#ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int     ft_printf(const char *str, ...);
-int     ft_params(char format, va_list args);
-int     ft_putptr(unsigned long long ptr);
-int     ft_putnbr_p(unsigned long long nb);
-int     ft_putstr(char *str);
+int		ft_printf(const char *str, ...);
+int		ft_params(char format, va_list args);
+int		ft_putptr(unsigned long long ptr);
+int		ft_putnbr_p(unsigned long long nb);
+int		ft_putstr(char *str);
 size_t	ft_strlen(const char *s);
-int     ft_putchar(char c);
-int     ft_putnbr_base(long long nb, char *base);
+int		ft_putchar(char c);
+int		ft_putnbr_base(long long nb, char *base);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
